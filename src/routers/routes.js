8db3bbb08312180a8a39ff52dafea454c199b112
Agivqy Nadif <agivqynadif@gmail.com>
2024-03-27 00:24:01 +0700

    fix: forgot password http request

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -6,7 +6,7 @@ const verifyToken = require('../middlewares/jwt-auth');
 module.exports = function (app) {
   app.post(`${process.env.API_URL}/auth/register`, verifyRegister.checkDuplicateUsername, userController.registerUser);
   app.post(`${process.env.API_URL}/auth/login`, userController.loginUser);
-  app.post(`${process.env.API_URL}/auth/forgot-password`, userController.forgotPassword);
+  app.put(`${process.env.API_URL}/auth/forgot-password`, userController.forgotPassword);
 
   app.put(`${process.env.API_URL}/user/reset-password`, verifyToken.authJwt, userController.resetPassword);
   app.put(`${process.env.API_URL}/user/profile-name/:id`, userController.updateProfileName);
