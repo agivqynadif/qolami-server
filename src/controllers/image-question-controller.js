35c81543eb767f88bc917b1fff50af56842fc20a
Agivqy Nadif <agivqynadif@gmail.com>
2024-05-10 16:59:41 +0700

    feat: add images practice question

--- src/controllers/image-question-controller.js
+++ src/controllers/image-question-controller.js
@@ -24,3 +24,78 @@ exports.insertImageQuestion = async (req, res) => {
     });
   }
 };
+
+exports.getLatihanHijaiyah = async (req, res) => {
+  try {
+    const latihanHijaiyah = await ImageQuestion.aggregate([{ $match: { title: 'Latihan 1' } }, { $sample: { size: 10 } }]);
+    res.status(200).json({
+      status: 'Sukses',
+      message: 'Berhasil mendapatkan soal latihan Huruf Hijaiyah!',
+      data: {
+        latihanHijaiyah,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: 'Error',
+      message: 'Terjadi error pada server',
+    });
+  }
+};
+exports.getLatihanFathah = async (req, res) => {
+  try {
+    const latihanHijaiyah = await ImageQuestion.aggregate([{ $match: { title: 'Latihan 2' } }, { $sample: { size: 10 } }]);
+    res.status(200).json({
+      status: 'Sukses',
+      message: 'Berhasil mendapatkan soal latihan Huruf Berharakat fathah!',
+      data: {
+        latihanHijaiyah,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: 'Error',
+      message: 'Terjadi error pada server',
+    });
+  }
+};
+
+exports.getLatihanKasrah = async (req, res) => {
+  try {
+    const latihanHijaiyah = await ImageQuestion.aggregate([{ $match: { title: 'Latihan 3' } }, { $sample: { size: 10 } }]);
+    res.status(200).json({
+      status: 'Sukses',
+      message: 'Berhasil mendapatkan soal latihan Huruf Berharakat Kasrah!',
+      data: {
+        latihanHijaiyah,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: 'Error',
+      message: 'Terjadi error pada server',
+    });
+  }
+};
+
+exports.getLatihanDhammah = async (req, res) => {
+  try {
+    const latihanHijaiyah = await ImageQuestion.aggregate([{ $match: { title: 'Latihan 4' } }, { $sample: { size: 10 } }]);
+    res.status(200).json({
+      status: 'Sukses',
+      message: 'Berhasil mendapatkan soal latihan Huruf Berharakat Dhammah!',
+      data: {
+        latihanHijaiyah,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: 'Error',
+      message: 'Terjadi error pada server',
+    });
+  }
+};
--- src/routers/image-question-routes.js
+++ src/routers/image-question-routes.js
@@ -1,7 +1,11 @@
-const { insertImageQuestion } = require('../controllers/image-question-controller');
+const { insertImageQuestion, getLatihanHijaiyah, getLatihanFathah, getLatihanKasrah, getLatihanDhammah } = require('../controllers/image-question-controller');
 const express = require('express');
 const router = express.Router();
 
 router.post('/images/insert-question', insertImageQuestion);
+router.get('/images-practice/hijaiyah', getLatihanHijaiyah);
+router.get('/images-practice/fathah', getLatihanFathah);
+router.get('/images-practice/kasrah', getLatihanKasrah);
+router.get('/images-practice/dhammah', getLatihanDhammah);
 
 module.exports = router;
